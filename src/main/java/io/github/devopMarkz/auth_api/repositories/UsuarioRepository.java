b2d65143538bfecc439ea7bc8a1e08987b52ea96
devopMarkz <marcosdev2002@gmail.com>
2025-01-23 20:28:35 -0300

    Criando implementacao de UserDetailsService

--- src/main/java/io/github/devopMarkz/auth_api/repositories/UsuarioRepository.java
+++ src/main/java/io/github/devopMarkz/auth_api/repositories/UsuarioRepository.java
@@ -3,7 +3,12 @@ package io.github.devopMarkz.auth_api.repositories;
 import io.github.devopMarkz.auth_api.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     boolean existsByLogin(String login);
+
+    Optional<Usuario> findByLogin(String login);
+
 }
