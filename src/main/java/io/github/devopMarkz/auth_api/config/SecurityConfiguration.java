72e093b9000e39545273613271159aff125894de
devopMarkz <marcosdev2002@gmail.com>
2025-01-24 20:05:42 -0300

    Adicionando filtro personalizado a cadeia de filtros de seguranca do Spring Security

--- src/main/java/io/github/devopMarkz/auth_api/config/SecurityConfiguration.java
+++ src/main/java/io/github/devopMarkz/auth_api/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package io.github.devopMarkz.auth_api.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,10 +13,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Autowired
+    private SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -28,6 +33,7 @@ public class SecurityConfiguration {
                     auth.requestMatchers(HttpMethod.POST, "/auth").permitAll();
                     auth.anyRequest().authenticated();
                 })
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
--- src/main/java/io/github/devopMarkz/auth_api/config/SecurityFilter.java
+++ src/main/java/io/github/devopMarkz/auth_api/config/SecurityFilter.java
@@ -1,4 +1,57 @@
 package io.github.devopMarkz.auth_api.config;
 
-public class SecurityFilter {
+import io.github.devopMarkz.auth_api.models.Usuario;
+import io.github.devopMarkz.auth_api.repositories.UsuarioRepository;
+import io.github.devopMarkz.auth_api.services.AutenticacaoService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@AllArgsConstructor
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private AutenticacaoService autenticacaoService;
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String token = extraiTokenHeader(request);
+
+        if(token != null){
+            String login = autenticacaoService.validarTokenJwt(token);
+            Usuario usuario = usuarioRepository.findByLogin(login).orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado."));
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String extraiTokenHeader(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+
+        if(authHeader == null){
+            return null;
+        }
+
+        // Bearer <Token>
+        if(!authHeader.split(" ")[0].equals("Bearer")){
+            return null;
+        }
+
+        return authHeader.split(" ")[1];
+    }
+
 }
