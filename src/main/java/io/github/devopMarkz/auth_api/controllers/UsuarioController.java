51accffba79d3e19c87d9f5c8e42bc5619fa6163
devopMarkz <marcosdev2002@gmail.com>
2025-01-27 17:39:23 -0300

    Adicionando cores no log

--- src/main/java/io/github/devopMarkz/auth_api/controllers/UsuarioController.java
+++ src/main/java/io/github/devopMarkz/auth_api/controllers/UsuarioController.java
@@ -2,6 +2,8 @@ package io.github.devopMarkz.auth_api.controllers;
 
 import io.github.devopMarkz.auth_api.dtos.UsuarioDTO;
 import io.github.devopMarkz.auth_api.services.UsuarioService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,11 +14,13 @@ import java.net.URI;
 @RequestMapping("/usuarios")
 public class UsuarioController implements AuxiliarController{
 
+    private static final Logger log = LoggerFactory.getLogger(UsuarioController.class);
     @Autowired
     private UsuarioService usuarioService;
 
     @PostMapping
     public ResponseEntity<UsuarioDTO> salvar(@RequestBody UsuarioDTO usuarioDTO){
+        log.info("Usuário salvo no banco de dados: {}", usuarioDTO.nome());
         var usuarioCriado = usuarioService.salvar(usuarioDTO);
         URI location = gerarURI(usuarioCriado.id());
         return ResponseEntity.created(location).body(usuarioCriado);
